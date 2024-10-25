2234a65ab4cba632c0372e2b9b6468e8e5e7a125
LoinRe <winard2@mail.ru>
2024-10-25 13:44:23 +0300

    Initial commit

---
+++ src/main/java/com/calculator/calcspringdemo/CalcSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.calcspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcSpringDemoApplication.class, args);
+	}
+
+}
